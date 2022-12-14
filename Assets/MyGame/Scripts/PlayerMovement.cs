9ca7dad43bca7fbd59838cc83763e981b0e2d2a3
TheGreenPig <72389363+TimToller@users.noreply.github.com>
2022-12-14 08:37:23 +0100

    Add Character controller

---
+++ Assets/MyGame/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float rotation;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        //rotate the player
+        transform.Rotate(0, 0, h * rotation);
+
+        //move the player in the direction it is facing
+        rb.AddForce(transform.right * acceleration * v);
+    }
+}
